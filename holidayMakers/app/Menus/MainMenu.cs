5fcbcf405395d054a75e7c194936588736c1499c
okzScripts <oskar.krantz90@gmail.com>
2024-11-26 19:17:34 +0100

    La till en Submenu

--- holidayMakers/app/Menus/MainMenu.cs
+++ holidayMakers/app/Menus/MainMenu.cs
@@ -5,6 +5,8 @@ namespace app.Menus;
 
 public class MainMenu
 {
+    
+    
     public MainMenu()
     {
 
@@ -13,7 +15,7 @@ public class MainMenu
 
 
     }
-
+    private SubMenu1 submenu1 = new SubMenu1();
     public void RunMenu(MainMenu mainMenu)
     {
         Console.ForegroundColor = ConsoleColor.Cyan;
@@ -33,7 +35,7 @@ public class MainMenu
             
             Console.WriteLine("\n👌Use the ⬆ and ⬇ to navigate, confirm by \u001b[32mEnter\u001b[0m.");
             Console.WriteLine($"{(option == 1 ? arrow : "    ")}   Option1\u001b[0m");
-            Console.WriteLine($"{(option == 2 ? arrow : "    ")}   Option2\u001b[0m");
+            Console.WriteLine($"{(option == 2 ? arrow : "    ")}   Submenu1\u001b[0m");
             Console.WriteLine($"{(option == 3 ? arrow : "    ")}   Option3\u001b[0m");
             Console.WriteLine($"{(option == 4 ? arrow : "    ")}   Option4\u001b[0m");
 
@@ -48,13 +50,22 @@ public class MainMenu
                     option = (option == 1 ? 4 : option-1);
                     break;
                 case ConsoleKey.Enter:
-                    Console.WriteLine("WIP");
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("WIP");
+                            break;
+                        case 2:
+                            Console.Clear();
+                            submenu1.RunMenu();
+                            break;
+                    }
                     run = false;
                     break;
             }
         }
 
-        Console.WriteLine($"You have selected option {option}.");
+        
         
 
     }
--- holidayMakers/app/Menus/SubMenu1.cs
+++ holidayMakers/app/Menus/SubMenu1.cs
@@ -1,11 +1,55 @@
 namespace app.Menus;
-/*
+
 public class SubMenu1
 {
     public SubMenu1()
     {
         
     }
-    
+
     public void RunMenu()
-}*/
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("THIS IS THE SUBMENU");
+        
+        
+        Console.ResetColor();
+
+        ConsoleKeyInfo key;
+        int option = 1;
+        bool run = true;
+        (int Left, int Top) = Console.GetCursorPosition();
+        string arrow ="===>\u001b[32m";
+        
+        while (run)
+        {
+            
+            Console.SetCursorPosition(Left,Top);
+            
+            Console.WriteLine("\n👌Use the ⬆ and ⬇ to navigate, confirm by \u001b[32mEnter\u001b[0m.");
+            Console.WriteLine($"{(option == 1 ? arrow : "    ")}   SubOption1\u001b[0m");
+            Console.WriteLine($"{(option == 2 ? arrow : "    ")}   SubOption2\u001b[0m");
+            Console.WriteLine($"{(option == 3 ? arrow : "    ")}   SubOption3\u001b[0m");
+            Console.WriteLine($"{(option == 4 ? arrow : "    ")}   SubOption4\u001b[0m");
+
+            key = Console.ReadKey(true);
+
+            switch (key.Key)
+            {
+                case ConsoleKey.DownArrow:
+                    option = (option == 4 ? 1 : option+1);
+                    break;
+                case ConsoleKey.UpArrow:
+                    option = (option == 1 ? 4 : option-1);
+                    break;
+                case ConsoleKey.Enter:
+                    Console.WriteLine("WIP");
+                    run = false;
+                    break;
+            }
+        }
+
+        Console.WriteLine($"You have selected option {option}.");
+        
+    }
+}
