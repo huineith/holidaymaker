03a2fccef9ee8eed69f0aeb585ee8ea32fdcfd3e
huineith <westholm.jacob@gmail.com>
2024-11-27 23:14:47 +0100

    add class Database and simple example in program.cs
    added class Database that creates a connection to the postgres database
    using the Npgsql package current file conntains my database inputs.
    Also added simple query example in program.cs to check that connection
    work

---
+++ holidayMakers/app/Database.cs
@@ -0,0 +1,27 @@
+namespace app;
+using Npgsql; 
+
+public class Database
+{
+   private readonly string _host = "localhost";
+   private readonly string _port = "5432"; 
+   private readonly string _username = "postgres";
+   private readonly string _password = "portedinme";
+   private readonly string _database = "makersofholidays";
+   private NpgsqlDataSource _connection;
+
+   public NpgsqlDataSource Connection()
+   {
+      return _connection; 
+   }
+
+   public Database()
+   {
+      _connection = NpgsqlDataSource.Create(
+            $"Host={_host};Port={_port};Username={_username};Password={_password};Database={_database}");
+      
+      using var conn = _connection.OpenConnection(); // Kontrollerar att vi har lyckats kopplat upp oss till databasen.
+   }
+
+
+}
--- holidayMakers/app/Program.cs
+++ holidayMakers/app/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using app;
+using Npgsql;
+
 Console.WriteLine("Hello, World!");
+
+Database mydb = new Database();
+var myconnection = mydb.Connection(); 
+
+    await using (var cmd= myconnection.CreateCommand("Select * from sights") )
+    await using (var reader = await cmd.ExecuteReaderAsync() )
+        while (await reader.ReadAsync())
+        {
+            Console.WriteLine($"id:{reader.GetInt32(0)}, "+
+                              $"sight:{reader.GetString(1)}, ");
+            
+        }
