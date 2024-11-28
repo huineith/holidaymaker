0125f396c68cc3bb09979c7459ac7669029c288e
jManniCode <jonas_manni@hotmail.com>
2024-11-28 09:45:48 +0100

    Added class Database
    added class database and code fon connection to it

---
+++ holidayMakers/app/Database.cs
@@ -0,0 +1,27 @@
+﻿namespace app;
+using Npgsql; 
+
+public class Database
+{
+    private readonly string _host = "localhost";
+    private readonly string _port = "5432"; 
+    private readonly string _username = "postgres";
+    private readonly string _password = "Sanooj14321";
+    private readonly string _database = "holidaymakers";
+    private NpgsqlDataSource _connection;
+
+    public NpgsqlDataSource Connection()
+    {
+        return _connection; 
+    }
+
+    public Database()
+    {
+        _connection = NpgsqlDataSource.Create(
+            $"Host={_host};Port={_port};Username={_username};Password={_password};Database={_database}");
+      
+        using var conn = _connection.OpenConnection(); // Kontrollerar att vi har lyckats kopplat upp oss till databasen.
+    }
+
+
+}
